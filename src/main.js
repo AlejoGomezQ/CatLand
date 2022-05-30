e9438306f238cd64895e797b9e9f4b0bfafb3342
Alejo Gomez <alejandro.gomezq97@gmail.com>
2022-05-30 15:31:32 -0500

    Limpiando el proyecto inicial

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+//libraries
+import "@/libraries/bootstrapVue";
+
 Vue.config.productionTip = false;
 
 new Vue({
